1841d7e2386e24747d46dfcd064d95d5093cf1a2
gguneii <guneysuleymanofa@gmail.com>
2024-10-12 01:02:02 +0400

    admin pop-up

--- js/index.js
+++ js/index.js
@@ -5,17 +5,29 @@ function sidebar() {
   mobSidebar.classList.toggle('hidden')
   deskSidebar.classList.toggle('top-[-100%]')
 }
+const popup = document.getElementById("popup");
 
+function handlePopup() {
+  popup.classList.toggle("!flex");
+}
+
+function preventClick(e) {
+  e.stopPropagation();
+}
 const title = document.getElementById('title')
 const file = document.getElementById('file')
 const view = document.getElementById('view')
+const date = document.getElementById('date')
 const description = document.getElementById('description')
+let XEBER = []
+
 
 function handleSubmit(event) {
   const titleVal = title.value
   const fileVal = file.value
-  const viewVal = view.value
   const descVal = description.value
+  const viewVal = view.value
+  const dateVal = date.value
   event.preventDefault()
 
   if (titleVal.trim().length < 5) return alert('Min 5, max 64 soz olmalidir')
@@ -23,6 +35,7 @@ function handleSubmit(event) {
     "title": titleVal,
     "img": fileVal,
     "view": viewVal,
+    "date": dateVal,
     "description": descVal,
   }
   fetch('https://6704e06b031fd46a830dbb27.mockapi.io/oxuaz', {
@@ -33,36 +46,40 @@ function handleSubmit(event) {
       },
     })
     .then((response) => response.json())
-    .then((json) => console.log(json));
-  getAllData()
+    .then((json) => {
+      console.log(json)
+      XEBER.length = 0
+      getAllData()
+      clearForm()
+    })
+  handlePopup()
 }
-let XEBER = []
 
 function getAllData() {
   fetch('https://6704e06b031fd46a830dbb27.mockapi.io/oxuaz')
     .then(res => res.json())
     .then(data => {
       XEBER.push(...data)
-      show()
+      handleTable()
       handleCard()
     })
 }
 getAllData()
 
-function deleteElan(id) {
+function deleteNews(id) {
   fetch(`https://6704e06b031fd46a830dbb27.mockapi.io/oxuaz/${id}`, {
       method: "DELETE"
     })
     .then(res => res.json())
-    .then(data => console.log(data))
-
-  XEBER = XEBER.filter(item => item.id != id)
-  show()
-
+    .then(data => {
+      console.log(data)
+      XEBER = XEBER.filter(item => item.id != id)
+      handleTable()
+    })
 }
 const elanlar = document.getElementById("elanlar")
 
-function show() {
+function handleTable() {
   if (elanlar) {
     elanlar.innerHTML = ''
     XEBER.map(item => {
@@ -72,10 +89,14 @@ function show() {
               <td class="py-2 sm:px-4 border-b">
                 <img src="${item.img}" alt="Image" class="w-16 h-16 object-cover">
               </td>
-              <td class="py-2 sm:px-4 border-b text-gray-800">250</td>
+              <td class="py-2 sm:px-4 border-b text-gray-800">${item.view}</td>
+              <td class="py-2 sm:px-4 border-b text-gray-800">${item.date}</td>
               <td class="py-2 sm:px-4 border-b text-gray-800">${item.description}</td>
               <td class="py-2 sm:px-4 border-b text-gray-800">
-                <button class="text-red-600 font-bold" onclick="deleteElan(${item.id})">X</button>
+                <i class="text-red-600 fa-solid fa-trash font-bold" onclick="deleteNews(${item.id})"></i>
+              </td>
+              <td class="py-2 sm:px-4 border-b text-gray-800">
+                <i class="text-black fa-solid fa-edit font-bold" onclick="editNews(${item.id})"></i>
               </td>
             </tr>
     `
@@ -88,8 +109,8 @@ const cards = document.getElementById('cards')
 function handleCard() {
   if (cards) {
     cards.innerHTML = ''
-  XEBER.map(item => {
-    cards.innerHTML += `
+    XEBER.map(item => {
+      cards.innerHTML += `
       <article class="card flex flex-col dark:bg-gray-50 rounded-md overflow-hidden">
               <a rel="noopener noreferrer" href="pages/content.htm?id=${item.id}"
                   aria-label="Te nulla oportere reprimique his dolorum">
@@ -129,6 +150,73 @@ function handleCard() {
         </article>
       
       `
-  })
+    })
   }
+}
+
+const form = document.getElementById("form")
+
+function editNews(id) {
+
+  const {
+    title: ad,
+    img,
+    description: desc,
+    view: baxis,
+    date: tarix
+  } = XEBER.find(item => item.id == id)
+
+  handlePopup()
+  title.value = ad
+  file.value = img
+  description.value = desc
+  view.value = baxis
+  date.value = tarix
+
+  form.onsubmit = (event) => {
+    event.preventDefault()
+    editEle(id)
+    form.onsubmit = handleSubmit
+  }
+}
+
+function editEle(id) {
+  const titleVal = title.value
+  const fileVal = file.value
+  const descVal = description.value
+  const viewVal = view.value
+  const dateVal = date.value
+  const obj = {
+    "title": titleVal,
+    "img": fileVal,
+    "view": +viewVal,
+    "date": dateVal,
+    "description": descVal,
+  }
+  console.log("edit eledm");
+  
+  fetch(`https://6704e06b031fd46a830dbb27.mockapi.io/oxuaz/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(obj),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      XEBER.length = 0
+      getAllData()
+      clearForm()
+    })
+    handlePopup()
+
+}
+
+function clearForm(){
+  title.value = '';
+  file.value = '';
+  description.value = '';
+  view.value = '';
+  date.value = '';
 }
